5281427719621c32bdf2c0abd2e70b55b7488856
KarimBlackStar <gavkops@gmail.com>
2025-11-21 12:09:57 +0100

    Añadido println en master

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main() {
 
-        System.out.println(String.format("Hello and welcome!"));
+        System.out.println("Hello and welcome!");
+        System.out.println("println desde master");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
