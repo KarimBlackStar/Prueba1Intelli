05a5776e55ebeb631f916dc4ebd10dd5b63939fe
KarimBlackStar <gavkops@gmail.com>
2025-11-21 12:18:02 +0100

    Commit pre-conflicto

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         System.out.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 15; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
